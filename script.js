bde0ff71a0d2c693b0bb853238fd4c0a7c960d7d
Dakodah Spurlin <dcspurlin@gmail.com>
2024-11-04 22:47:34 -0600

    Got one button working correctly.

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let fungusHP = 100 //total HP
+let playerAP = 100
+let arcaneScepterAtk = 14 //hp dmg 
+let entangle = 9 //hp dmg
+let dragonBlade = 47 //hp dmg
+let starFire = 23 //hp dmg
+
+function arcaneScepter(){
+    let fungusTotal = fungusHP
+
+    fungusHP =  fungusTotal - arcaneScepterAtk
+    console.log('Fungus remaining HP = ', fungusHP)
+    render()
+    }
+
 
 function onReady() {
     console.log("Ready to go!")
@@ -15,6 +30,12 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
-
+function render() {
+    const hpText = document.querySelector('.hp-text')
+    const apText = document.querySelector('.ap-text')
+    console.log('Testing new Variable', fungusHP)
+    hpText.innerText = fungusHP + ' HP'
+    
+}
 
 onReady()
